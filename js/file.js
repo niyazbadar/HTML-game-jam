f2e65134e1fb47749fe954c7e51b3130dc1f31c8
niyazbadar <67091185+niyazbadar@users.noreply.github.com>
2023-01-29 16:46:38 +0530

    full game

--- js/file.js
+++ js/file.js
@@ -5,13 +5,13 @@ let road = document.querySelector("#road");
 let cloud = document.querySelector("#cloud");
 let score = document.querySelector("#score");
 let gameOver = document.querySelector("#gameOver");
+let started = document.querySelector("#started");
 
 //declaring variable for score
 let interval = null;
 let playerScore = 0;
 let point = 1;
 let active = true;
-
 
 //function for score
 let scoreCounter = () => {
@@ -24,9 +24,10 @@ let scoreCounter = () => {
 window.addEventListener("keydown", (start) => {
     //    console.log(start);
     if (start.code == "Space" && active) {
+        started.style.display = "none";
         gameOver.style.display = "none";
         block.classList.add("blockActive");
-        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        road.firstElementChild.style.animation = "roadAnimate 2s linear infinite";
         cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
 
         active = false;
